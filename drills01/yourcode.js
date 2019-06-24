4dfa7be2bc69657bb62c91394f6aad209c37194c
Jase Thomson <jasethomson31@gmail.com>
2019-06-24 08:14:08 -0700

    Drill3 Question 1 Done, Drill1: Questions 1 and 2 done

--- drills01/yourcode.js
+++ drills01/yourcode.js
@@ -1,10 +1,34 @@
-
-function sumArray(){
-	
+function sumArray(array){
+  var index = 0;
+  var sum = 0;
+  while(index<array.length){
+  sum += array[index];
+  console.log(sum);
+  index++;
+  }
+  return sum;
 }
 
-function fitWithinVal(){
-
+function fitWithinVal(array, num){
+  var index = 0;
+  var sum = 0;
+  var newArray = [];
+  while(sum < num) {
+    sum += array[index];
+    if (sum < num){
+      newArray.push(array[index]);
+      console.log('sum ', sum);
+      console.log('newArray ',newArray);
+    } else if(sum > num) {
+      sum -= array[index];
+      console.log('sum ', sum);
+      console.log('newArray ', newArray);
+    }
+    else {
+      return newArray;
+    }
+    index++;
+  }
 }
 
 function getAllNamesShorterThan(){
--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -20,27 +20,6 @@ function getPath(string){
   return newArray;
 }
 
-
-/*
-var testVals = [{
-  question: `getPath: given a path string, return an array with the different parts of the path
-    for example: 'folder1/folder2/folder3'
-        returns ['folder1','folder2','folder3']
-  `,
-  testVals: {
-    inputs: [
-        ['var/www/html'],
-        ['stuff/thing/stuff/howdy'],
-
-    ],
-    outputs:
-    [  ['var','www','html'],
-       ['stuff','thing','stuff','howdy'],
-    ]
-  },
-  functionToTest: 'getPath'
-  */
-
 function getPathParts(){
 
 }
