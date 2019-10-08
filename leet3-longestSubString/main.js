c81f74183ee22fe3fe2b05b87232d6e5a953ab0a
jasethomson <jasethomson31@gmail.com>
2019-10-07 22:36:16 -0700

    Working on finishing leet3, need sleep!

--- leet3-longestSubString/main.js
+++ leet3-longestSubString/main.js
@@ -1,3 +1,40 @@
-function subStringGen(string){
 
+function subStringGen(string){
+  if(string){
+    let letterArray = [string[0]];
+    let longestArray = [];
+    let outerCount = 1;
+    let loopCount = 0;
+    while (loopCount < string.length) {
+      let innerCount = 0;
+      let match = false;
+      while (innerCount < letterArray.length) {
+        if (string[outerCount] === letterArray[innerCount]) {
+          if (letterArray.length > longestArray.length) {
+            longestArray = letterArray;
+          }
+          letterArray = [];
+          match = true;
+          loopCount++;
+          outerCount = loopCount;
+          break;
+        }
+        if (letterArray.length > longestArray.length) {
+          longestArray = letterArray;
+        }
+        innerCount++;
+      }
+      if (!match) {
+        letterArray.push(string[outerCount]);
+        match = false;
+        outerCount++;
+      }
+      outerCount++;
+    }
+    if (longestArray.length < letterArray.length) {
+      return letterArray.length;
+    }
+    return longestArray.length;
+  }
+  return 0;
 }
