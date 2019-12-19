87c6c30ab662dbfc505a432d8b8d50058b38d7b4
jasethomson <jasethomson31@gmail.com>
2019-12-19 08:08:46 -0800

    completed pig latin problem

--- practice07/main.js
+++ practice07/main.js
@@ -1,20 +1,25 @@
-function translate(string){
-  var stringToReturn = "";
-  var firstLetter = string[0];
-  var storeFirstLetter = string[0];
-  for(var character = 1; character < string.length + 1; character++){
-    // debugger;
-    if(string[character] === " "){
-      stringToReturn += firstLetter + "ay ";
-      firstLetter = string[character + 1];
-      character += 1;
-    } else if(string[character] === undefined) {
-      stringToReturn += firstLetter + "ay ";
-    } else {
-      stringToReturn += string[character];
-    }
-  }
-  return stringToReturn;
+function translate(str){
+  str += " ";
+  let subStr = "";
+  let char1;
 
+  let index2 = 0;
+  while(index2 < str.length-1){
+    char1 = str[index2];
+    index2++;
+    while (str[index2] !== " ") {
+      subStr += str[index2++];
+    }
+    index2++;
+    if (index2 < str.length - 1){
+      subStr += char1 + "ay ";
+    } else {
+      subStr += char1 + "ay";
+    }
+
+  }
+
+  console.log("str:",subStr);
 }
 translate("Hello my name is Stu");
+translate("Hello there");
