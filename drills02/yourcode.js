ee5267c6d268b172abcafce85c716f82d001fb12
jasethomson31@gmail.com <jasethomson31@gmail.com>
2021-09-28 23:40:28 -0700

    completed drills 2

--- drills02/yourcode.js
+++ drills02/yourcode.js
@@ -7,14 +7,12 @@
 
 */
 
-function countOccurences(wordsArray, separateWord){
-  var countNumberOfOccurences = 0;
-  for(var arrayAtReference = 0;arrayAtReference<wordsArray.length; arrayAtReference++) {
-    if (wordsArray[arrayAtReference] === separateWord){
-      countNumberOfOccurences++;
-    }
-  }
-  return countNumberOfOccurences;
+function countOccurences(wordsArray, separateWord) {
+  let count = 0;
+  wordsArray.map(word => {
+    if (word === separateWord) count++;
+  });
+  return count;
 }
 /*
 - wordLengths:
@@ -23,37 +21,30 @@ function countOccurences(wordsArray, separateWord){
   - return [6,5,3,1,6]
 */
 
-function wordLengths(arrayOfWords){
-  var newArrayOfNumbers = [];
-  for(var arrayCounter = 0; arrayCounter<arrayOfWords.length; arrayCounter++){
-    var lengthOfArray = arrayOfWords[arrayCounter].length;
-    newArrayOfNumbers.push(lengthOfArray);
-  }
-  return newArrayOfNumbers;
+function wordLengths(arrayOfWords) {
+  let arr = [];
+  arrayOfWords.map(word => arr.push(word.length));
+  return arr;
 }
 /*
 - getMinMaxMean: given an array of numbers, get the minimum, maximum, and mean numbers. Return them as an object. Mean is another word for average For example:
 	- input [3,1,9,12,4]
   - return: {min: 1, max: 12, mean: 25.8}
 */
-function getMinMaxMean(arrayOfNumbers){
-  var numberObject = {};
-  var maxNum = arrayOfNumbers[0];
-  var minNum = arrayOfNumbers[0];
-  var sumNums = 0;
-  for ( var numberChecker = 0; numberChecker<arrayOfNumbers.length; numberChecker++){
-    sumNums += arrayOfNumbers[numberChecker];
-    if (arrayOfNumbers[numberChecker]>maxNum){
-      maxNum = arrayOfNumbers[numberChecker];
-    } else if (arrayOfNumbers[numberChecker]<minNum){
-      minNum = arrayOfNumbers[numberChecker];
-    }
-  }
-  var mean = sumNums / arrayOfNumbers.length;
-  numberObject.min = minNum;
-  numberObject.max = maxNum;
-  numberObject.mean = mean;
-  return numberObject;
+function getMinMaxMean(arrayOfNumbers) {
+  const deets = {
+    min: arrayOfNumbers[0],
+    max: arrayOfNumbers[0],
+    mean: 0
+  };
+  let total = 0;
+  arrayOfNumbers.map(num => {
+    if (num > deets.max) deets.max = num;
+    if (num < deets.min) deets.min = num;
+    total += num;
+  });
+  deets.mean = total / arrayOfNumbers.length;
+  return deets;
 }
 /*
 - findMode: Given an array of numbers, find the number that occurs the most often for example:
@@ -61,25 +52,18 @@ function getMinMaxMean(arrayOfNumbers){
   - returns 2, since it occurs 3 times. If 2 numbers occur the same amount, return the last one
 */
 
-function findMode(arrayNumbers){
-  var numberCounter = 0;
-  var occursTheMost = 0;
-  var stringNumTheMost = 0;
-  for (var numberOfNumbers = 0; numberOfNumbers < arrayNumbers.length; numberOfNumbers++ ){
-    var numToCheck = arrayNumbers[numberOfNumbers];
-    for (var numChecker = 0; numChecker < arrayNumbers.length; numChecker++) {
-      if (numToCheck === arrayNumbers[numChecker]){
-        numberCounter++;
-      }
+function findMode(arrayNumbers) {
+  const modeObj = {};
+  arrayNumbers.map(num => {
+    typeof modeObj[num] === "number" ? modeObj[num] += 1 : modeObj[num] = 1;
+  });
+  let occurs = 0;
+  let mode = arrayNumbers[0];
+  for (const num in modeObj) {
+    if (modeObj[num] > occurs) {
+      occurs = modeObj[num];
+      mode = num;
     }
-    if (numberCounter>occursTheMost){
-      occursTheMost = numberCounter;
-      stringNumTheMost = numToCheck;
-    } else if (numberCounter === occursTheMost){
-      occursTheMost = numberCounter;
-      stringNumTheMost = numToCheck;
-    }
-    numberCounter = 0;
   }
-  return stringNumTheMost;
+  return parseInt(mode);
 }
