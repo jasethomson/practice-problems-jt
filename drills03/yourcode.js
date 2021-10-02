1d6929efb4b384869693a4975342c0a54e84fa9c
jasethomson31@gmail.com <jasethomson31@gmail.com>
2021-10-01 23:33:53 -0700

    completed first 2 funcs in drills 3

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -4,35 +4,63 @@
 	- returns ['folder1','folder2','folder3']
 */
 
-function getPath(pathString){
-  var newPathArray = [];
-  var stringHolder = '';
-  var lastString = '';
-  var pathContainer = pathString;
-  while(pathContainer.length>0) {
-    if(pathContainer.indexOf('/')>0){
-      stringHolder = pathContainer.slice(pathContainer.lastIndexOf('/'));
-      newPathArray.push(stringHolder);
-    } else {
-      lastString = pathContainer;
-      newPathArray.push(lastString);
-    }
-    console.log(newPathArray);
+
+//
+
+function getPath(pathString) {
+  return pathString.split("/");
+}
+
+// - getPathParts: Given a full URL string, break it up into parts in an object For
+//   - example: 'http://localhost:8000/happy/go/lucky/file.html'
+//     - return { protocol: 'http', host: 'localhost', port: 8000, path: '/happy/go/lucky', file: 'file.html' }
+
+function getPathParts(url) {
+  const protocol = url.split(':')[0];
+  const host = (url.split('/')[2]).split(":")[0];
+  const port = parseInt((url.split(":")[2]).split('/')[0]);
+
+  let pathWithFileSplitOnPeriod = ((url.split(":")[2]).replace(port, "")).split(".");
+  let pathWithFileArr = pathWithFileSplitOnPeriod[0].split('/');
+  pathWithFileArr.shift();
+  const file = `${pathWithFileArr.pop()}.${pathWithFileSplitOnPeriod[1]}`;
+  let path = "";
+  pathWithFileArr.map(folder => path += `/${folder}`);
+  path = path.slice(1);
+
+  return {
+    protocol,
+    host,
+    port,
+    path,
+    file
   }
 }
 
-function getPathParts(){
+//       - getCapitalCount: given an array of words, return a count of how many start with capital letters, For
+//         - example: ['it', 'will', 'not', 'Be', 'long', 'Till', 'we', 'Are']
+//           - returns: 3
+
+//             - correctCalcChecker: Given an array of objects with math equations, return all objects that have correct equations for
+// 	- example: [
+//     { num1: 3, num2: 3, op: '/', result: 3 },
+//     { num1: 12, num2: 4, op: '-', result: 8 },
+//     { num1: 2, num2: 3, op: '+', result: 5 },
+//     { num1: -5, num2: -2, op: '*', result: 10 }
+//   ]
+//     - returns[
+//     { num1: 12, num2: 4, op: '-', result: 8 },
+//     { num1: 2, num2: 3, op: '+', result: 5 }
+//     ]
+
+function getCapitalCount() {
 
 }
 
-function getCapitalCount(){
+function correctCalcChecker() {
 
 }
 
-function correctCalcChecker(){
-
-}
-
-function doMath(){
+function doMath() {
 
 }
