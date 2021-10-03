f90baa92ca281e4900e4828c0256b040b20601bf
jasethomson31@gmail.com <jasethomson31@gmail.com>
2021-10-02 22:23:49 -0700

    completed practice problem 31

--- practice31/main.js
+++ practice31/main.js
@@ -1,11 +1,5 @@
 
-function cocococombomath(num1, num2, num3, num4, num5){
-  let result = num1;
-  result *= num2;
-  result += num3;
-  result /= num4;
-  result -= num5;
-  return result;
+function cocococombomath(num1, num2, num3, num4, num5) {
+  return ((((num1 * num2) + num3) / num4) - num5);
 }
-console.log("result:", cocococombomath(3,2,6,3,5));
-
+console.log("result:", cocococombomath(3, 2, 6, 3, 5));
